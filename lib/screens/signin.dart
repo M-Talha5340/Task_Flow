9ac98464dbd61f171d354fbb2c4e6abfacb5f26d
M-Talha5340 <muhammadtalha534024@gmail.com>
2026-07-06 11:57:39 +0500

    Add Splash And Sign In screen

---
+++ lib/screens/signin.dart
@@ -0,0 +1,316 @@
+import 'package:flutter/material.dart';
+
+class Signin extends StatefulWidget {
+  const Signin({super.key});
+
+  @override
+  State<Signin> createState() => _SigninState();
+}
+
+class _SigninState extends State<Signin> {
+ bool rememberMe = false;
+  bool obscurePassword = true;
+  var emailController = TextEditingController();
+  var passwordController = TextEditingController();
+  final _formkey = GlobalKey<FormState>();
+
+  String? validatePassword(String? val) {
+    if (val!.isEmpty) {
+      return "Enter Password";
+    } else {
+      bool hasUppercase = false;
+      bool hasLowercase = false;
+      bool hasDigit = false;
+      bool hasSpecialChar = false;
+      for (int i = 0; i < val.length; i++) {
+        String char = val[i];
+
+        if (char.contains(RegExp(r'[A-Z]'))) {
+          hasUppercase = true;
+        }
+        if (char.contains(RegExp(r'[a-z]'))) {
+          hasLowercase = true;
+        }
+        if (char.contains(RegExp(r'[0-9]'))) {
+          hasDigit = true;
+        }
+        if (char.contains(RegExp(r'[@$!%#^*?&]'))) {
+          hasSpecialChar = true;
+        }
+      }
+      if (val.length < 4) {
+        return "Must be atleast 4 characters";
+      }
+      if (!hasUppercase) {
+        return "Must contain at least one uppercase letter";
+      }
+
+      if (!hasLowercase) {
+        return "Must contain at least one lowercase letter";
+      }
+
+      if (!hasDigit) {
+        return "Must contain at least one number";
+      }
+
+      if (!hasSpecialChar) {
+        return "Must contain at least one special character";
+      }
+    }
+    return null;
+  }
+  String? isValidEmail(String? email) {
+  final RegExp emailRegex = RegExp(
+    r'^[a-zA-Z0-9._%+-]+@[a-zA-Z0-9.-]+\.[a-zA-Z]{2,}$',
+  );
+    if (email == null){
+      return "Enter Email";
+    }
+    if(emailRegex.hasMatch(email)){
+         return null;
+    }
+    return "Invalid Email Formate";
+}
+
+
+  @override
+  Widget build(BuildContext context) {
+    return Scaffold(
+      backgroundColor: const Color(0xffF6F7FC),
+      body: Form(
+        key: _formkey,
+        child: SafeArea(
+          child: GestureDetector(
+          behavior: HitTestBehavior.opaque,
+          onTap: () {
+            FocusManager.instance.primaryFocus?.unfocus();
+          },
+           child: 
+          Center(
+            child: SingleChildScrollView(
+              padding: const EdgeInsets.all(20),
+              child: Container(
+                width: double.infinity,
+                padding: const EdgeInsets.symmetric(
+                  horizontal: 20,
+                  vertical: 20,
+                ),
+                decoration: BoxDecoration(
+                  color: Colors.white,
+                  borderRadius: BorderRadius.circular(25),
+                  border: Border.all(
+                    color: Colors.grey.shade300,
+                  ),
+                ),
+                child: Column(
+                  crossAxisAlignment: CrossAxisAlignment.center,
+                  children: [
+                        /// Logo
+                    Container(
+                      height: 90,
+                      width: 90,
+                      decoration: BoxDecoration(
+                        color: const Color(0xffDDE4FF),
+                        borderRadius: BorderRadius.circular(25),
+                      ),
+                      child: const Icon(
+                        Icons.lock_person_outlined,
+                        size: 45,
+                        color: Color(0xff0D47A1),
+                      ),
+                    ),
+        
+                    const SizedBox(height: 15),
+        
+                    const Text(
+                      "Welcome Back",
+                      style: TextStyle(
+                        fontSize: 34,
+                        fontWeight: FontWeight.bold,
+                        color: Color(0xff102A5C),
+                      ),
+                    ),
+        
+                    const SizedBox(height: 5),
+        
+                    Text(
+                      "Please enter your details to signIn",
+                      style: TextStyle(
+                        color: Colors.grey.shade700,
+                        fontSize: 18,
+                      ),
+                    ),
+        
+                    const SizedBox(height: 25),
+        
+                    Align(
+                      alignment: Alignment.centerLeft,
+                      child: Text(
+                        "Email Address",
+                        style: TextStyle(
+                          fontWeight: FontWeight.w600,
+                          fontSize: 18,
+                          color: Colors.grey.shade800,
+                        ),
+                      ),
+                    ),
+        
+                    const SizedBox(height: 10),
+        
+                    TextFormField(
+                       controller: emailController,
+                       validator: isValidEmail,
+                      decoration: InputDecoration(
+                        hintText: "abc@company.com",
+                        prefixIcon: const Icon(Icons.mail_outline),
+                        contentPadding:
+                            const EdgeInsets.symmetric(vertical: 18),
+                        border: OutlineInputBorder(
+                          borderRadius: BorderRadius.circular(15),
+                        ),
+                      ),
+                    ),
+        
+                    const SizedBox(height: 15),
+        
+                    Row(
+                      mainAxisAlignment: MainAxisAlignment.spaceBetween,
+                      children: [
+                        Text(
+                          "Password",
+                          style: TextStyle(
+                            fontWeight: FontWeight.w600,
+                            fontSize: 18,
+                            color: Colors.grey.shade800,
+                          ),
+                        ),
+                        TextButton(
+                          onPressed: () {},
+                          child: const Text(
+                            "Forgot Password?",
+                            style: TextStyle(
+                              color: Color(0xff0D47A1),
+                            ),
+                          ),
+                        )
+                      ],
+                    ),
+        
+                    TextFormField(
+                      obscureText: obscurePassword,
+                      validator: validatePassword,
+                      controller: passwordController,
+                      decoration: InputDecoration(
+                        hintText: "Ab@123",
+                        prefixIcon: const Icon(Icons.lock_outline),
+                        suffixIcon: IconButton(
+                          icon: Icon(
+                            obscurePassword
+                                ?Icons.visibility_off_outlined:Icons.visibility_outlined,
+                          ),
+                          onPressed: () {
+                            setState(() {
+                              obscurePassword = !obscurePassword;
+                            });
+                          },
+                        ),
+                        border: OutlineInputBorder(
+                          borderRadius: BorderRadius.circular(15),
+                        ),
+                      ),
+                    ),
+        
+                    const SizedBox(height: 18),
+        
+                    Row(
+                      children: [
+                        Checkbox(
+                          value: rememberMe,
+                          onChanged: (value) {
+                            setState(() {
+                              rememberMe = value!;
+                            });
+                          },
+                        ),
+                        const Text(
+                          "Keep me signed in",
+                          style: TextStyle(fontSize: 17),
+                        )
+                      ],
+                    ),
+        
+                    const SizedBox(height: 15),
+        
+                    SizedBox(
+                      width: double.infinity,
+                      height: 60,
+                      child: ElevatedButton(
+                        style: ElevatedButton.styleFrom(
+                          backgroundColor: const Color(0xff0D47A1),
+                          shape: RoundedRectangleBorder(
+                            borderRadius: BorderRadius.circular(15),
+                          ),
+                        ),
+                        onPressed: () {
+                            if(_formkey.currentState!.validate()){
+                                  print("Valid");
+                            }
+
+                        },
+                        child: const Row(
+                          mainAxisAlignment: MainAxisAlignment.center,
+                          children: [
+                            Text(
+                              "Login",
+                              style: TextStyle(
+                                fontSize: 26,
+                                color: Colors.white,
+                                fontWeight: FontWeight.bold,
+                              ),
+                            ),
+                            SizedBox(width: 10),
+                            Icon(
+                              Icons.arrow_forward,
+                              color: Colors.white,
+                              size: 30,
+                            )
+                          ],
+                        ),
+                      ),
+                    ),
+        
+                    const SizedBox(height: 15),
+        
+                    Row(
+                      mainAxisAlignment: MainAxisAlignment.center,
+                      children: [
+                        Text(
+                          "Don't have an account?",
+                          style: TextStyle(
+                            color: Colors.grey.shade700,
+                            fontSize: 18,
+                          ),
+                        ),
+                        TextButton(
+                          onPressed: () {},
+                          child: const Text(
+                            "Sign Up",
+                            style: TextStyle(
+                              color: Color(0xff0D47A1),
+                              fontWeight: FontWeight.bold,
+                              fontSize: 18,
+                            ),
+                          ),
+                        )
+                      ],
+                    )
+                  ],
+                ),
+              ),
+            ),
+          ),
+        ),
+      ),
+    ));
+  }
+}
---
+++ lib/screens/splash.dart
@@ -0,0 +1,100 @@
+import 'dart:async';
+
+import 'package:flutter/material.dart';
+import 'package:task_flow/screens/signIn.dart';
+
+class SplashScreen extends StatefulWidget {
+  const SplashScreen({super.key});
+
+  @override
+  State<SplashScreen> createState() => _SplashScreenState();
+}
+
+class _SplashScreenState extends State<SplashScreen> {
+
+     @override
+     void initState(){
+        super.initState();
+         Timer(const Duration(seconds: 3), () {
+      Navigator.pushReplacement(
+        context,
+        MaterialPageRoute(
+          builder: (context) => const Signin(),
+        ),
+      );
+    });
+     }
+  @override
+  Widget build(BuildContext context) {
+    return Scaffold(
+      backgroundColor: const Color(0xffF7F8FC),
+      body: Center(
+        child: Column(
+          mainAxisAlignment: MainAxisAlignment.center,
+          children: [            
+            // Logo Card
+            Container(
+              width: 100,
+              height: 100,
+              decoration: BoxDecoration(
+                color: Colors.white,
+                borderRadius: BorderRadius.circular(28),
+                boxShadow: [
+                  BoxShadow(
+                    color: Colors.blue.withValues(alpha: 0.08),
+                    blurRadius: 20,
+                    offset: const Offset(0, 10),
+                  ),
+                ],
+              ),
+              child: Center(
+                child: Container(
+                  width: 48,
+                  height: 48,
+                  decoration: BoxDecoration(
+                    border: Border.all(
+                      color: const Color(0xffAFC4FF),
+                      width: 3,
+                    ),
+                    borderRadius: BorderRadius.circular(24),
+                  ),
+                  child: const Icon(
+                    Icons.check,
+                    color: Color(0xffAFC4FF),
+                    size: 30,
+                  ),
+                ),
+              ),
+            ),
+        
+            const SizedBox(height: 25),
+        
+            // App Name
+            const Text(
+              "TaskFlow",
+              style: TextStyle(
+                fontSize: 32,
+                fontWeight: FontWeight.bold,
+                color: Color(0xff0D47A1),
+              ),
+            ),
+        
+            const SizedBox(height: 8),
+        
+            // Subtitle
+            const Text(
+              "PRECISION PRODUCTIVITY",
+              style: TextStyle(
+                fontSize: 18,
+                letterSpacing: 3,
+                color: Colors.grey,
+                fontWeight: FontWeight.w500,
+              ),
+            ),
+        
+          ],
+        ),
+      ),
+    );
+  }
+}
